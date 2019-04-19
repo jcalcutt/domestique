45cb0c33f3fc8f022f0ead756d6278ca126a663d
jcalcutt <jcalcutt7@gmail.com>
2019-04-19 10:58:36 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='domestique',
+    version='0.1dev',
+    python_requires='~=3.7',
+    packages=['tests',
+              'domestique',
+              'domestique.util'],
+    url='https://github.com/jcalcutt/domestique',
+    license='GNU General Public License v3.0',
+    author='Jordan C',
+    author_email='jcalcutt7<at>gmail.com',
+    description=''
+)
